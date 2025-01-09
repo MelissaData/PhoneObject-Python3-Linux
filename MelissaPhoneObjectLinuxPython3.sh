6b43adea21f13a91f85f3ea17633c5d0ac99e1c5
Melissa <>
2025-01-09 13:19:53 -0800

    Tested successfully using Melissa data files for 2025.01

--- MelissaPhoneObjectLinuxPython3.sh
+++ MelissaPhoneObjectLinuxPython3.sh
@@ -52,7 +52,7 @@ while [ $# -gt 0 ] ; do
 done
 
 # ######################### Config ###########################
-RELEASE_VERSION='2024.12'
+RELEASE_VERSION='2025.01'
 ProductName="DQ_PHONE_DATA"
 
 # Uses the location of the .sh file 
